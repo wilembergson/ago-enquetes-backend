b05b1afff36cae51bd0534a713ae6089178afebe
Wilembergson Alfredo da Silva <56082029+wilembergson@users.noreply.github.com>
2023-11-19 17:31:00 -0300

    fix: liberando cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './ioc/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(5000);
 }
 bootstrap();
